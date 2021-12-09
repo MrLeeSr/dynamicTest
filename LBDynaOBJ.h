3f6acb65e08db3920700fe99940a0ab09d582290
李斌 <libin11@corp.netease.com>
2021-12-09 17:21:44 +0800

    添加代码

---
+++ LBDynaOBJ.h
@@ -0,0 +1,16 @@
+//
+//  LBOBJ.h
+//  testFPSDK
+//
+//  Created by 李斌 on 2021/11/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LBDynaOBJ : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LBDynaOBJ.m
@@ -0,0 +1,12 @@
+//
+//  LBOBJ.m
+//  testFPSDK
+//
+//  Created by 李斌 on 2021/11/11.
+//
+
+#import "LBDynaOBJ.h"
+
+@implementation LBDynaOBJ
+
+@end
